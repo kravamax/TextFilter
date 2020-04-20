1b8aa2d2baade5d415749980225047494a9e2571
Maxim Kravchenko <maks.cross@gmail.com>
2020-04-20 18:28:32 +0300

    Add status buttons which activate filters

--- CPP_Exam/main.cpp
+++ CPP_Exam/main.cpp
@@ -4,6 +4,16 @@
 
 using namespace std;
 
+void StatusAction(bool status) {
+	if (status == true) 
+	{ 
+		cout << "\tON" << endl; 
+	}
+	else if (status == false)
+	{
+		cout << "\tOFF" << endl;
+	}
+}
 
 int main() {
 
@@ -15,18 +25,22 @@ int main() {
 
 	cin.getline(strArray, 100);
 
-	int n = strlen(strArray);
+	int length = strlen(strArray);
 
-	/*char* strArray_copy = new char[n];
+	/*char* strArray_copy = new char[length + 1];
 
-	for (int i = 0; i < n; i++)
+	for (int i = 0; i < length; i++)
 	{
 		strArray_copy[i] = strArray[i];
-	}
+	}*/
 	
-	cout << strArray << endl;
-	cout << strArray_copy << endl;*/
+	//cout << strArray << endl;
+	//cout << strArray_copy << endl;
 
+	bool status1 = false;
+	bool status2 = false;
+	bool status3 = false;
+	bool status4 = false;
 	
 	int action;
 
@@ -34,43 +48,44 @@ int main() {
 	{
 		cout << strArray;
 
-		cout << "\n1 - latin characters " << endl;
-		cout << "2 - cyrillic characters " << endl;
-		cout << "3 - punctuation characters " << endl;
-		cout << "4 - numbers " << endl;
+		cout << "\n1 - latin "; StatusAction(status1);
+		cout << "2 - cyrillic "; StatusAction(status2);
+		cout << "3 - signs "; StatusAction(status3);
+		cout << "4 - numbers "; StatusAction(status4);
 		cin >> action;
 
 		switch (action)
 		{
-		case 1: for (int i = 0; i < n; i++)
-				{
-					if (strArray[i] > 64 && strArray[i] < 91 || strArray[i] > 96 && strArray[i] < 123)				// latin characters
-					{
-						strArray[i] = '*';
-					}
-				} break;
-		case 2: for (int i = 0; i < n; i++)
+		case 1: for (int i = 0; i < length; i++)
+		{
+			if (strArray[i] > 64 && strArray[i] < 91 || strArray[i] > 96 && strArray[i] < 123)				// latin characters
+			{
+				strArray[i] = '*';
+			}
+		} 
+			  status1 = true; break;
+		case 2: for (int i = 0; i < length; i++)
 				{
 					if (strArray[i] > 192 && strArray[i] <=255)				// cyrillic characters
 					{
 						strArray[i] = '*';
 					}
-				}  break;
-		case 3:  for (int i = 0; i < n; i++)
+				} status2 = true; break;
+		case 3:  for (int i = 0; i < length; i++)
 				{
 					if (strArray[i] > 0 && strArray[i] <= 47 || strArray[i] >= 58 && strArray[i] <= 64 ||
 						strArray[i] >= 91 && strArray[i] <= 96 || strArray[i] >= 123 && strArray[i] <= 191)				// other symbols
 					{
 						strArray[i] = '*';
 					}
-				}  break;
-		case 4: for (int i = 0; i < n; i++)
+				} status3 = true; break;
+		case 4: for (int i = 0; i < length; i++)
 				{
 					if (strArray[i] > 47 && strArray[i] < 58)				//numbers
 					{
 						strArray[i] = '*';
 					}
-				}  break;
+				} status4 = true; break;
 		default:
 			break;
 		}
